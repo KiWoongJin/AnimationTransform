1e05b44e986cdaac036d81ad9e5135a93ee91980
KiWoongJin <jkw87first@naver.com>
2014-01-08 09:21:08 +0900

    AnimationTransform
    p231

--- AnimationTransform/ViewController.m
+++ AnimationTransform/ViewController.m
@@ -9,10 +9,60 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
+- (IBAction)series:(id)sender {
+    [UIView beginAnimations:@"series1" context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    
+    self.imageView.backgroundColor = [UIColor whiteColor];
+    
+    [UIView commitAnimations];
+    
+}
+- (IBAction)setOriginal:(id)sender {
+    [UIView beginAnimations:@"original" context:nil];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imageView.transform = CGAffineTransformIdentity;
+    
+    [UIView commitAnimations];
+    
+}
+- (IBAction)combination:(id)sender {
+    CGAffineTransform t1 = CGAffineTransformMakeScale(1.2, 1.2);
+    CGAffineTransform t2 = CGAffineTransformMakeTranslation(30, 30);
+    CGAffineTransform comb = CGAffineTransformConcat(t1, t2);
+    CGAffineTransform t = CGAffineTransformConcat(self.imageView.transform, comb);
+    
+    [UIView beginAnimations:@"id_move" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imageView.transform = t;
+    
+    [UIView commitAnimations];
+}
+- (IBAction)rotate:(id)sender {
+    [UIView beginAnimations:@"id_rotate" context:nil];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+    [UIView setAnimationDuration:0.7];
+    
+    self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, M_PI);
+    
+    [UIView commitAnimations];
+}
+
+
+
+
+
 
 - (void)viewDidLoad
 {
